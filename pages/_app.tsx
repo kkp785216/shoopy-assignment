2394ca23f812bb2854c8d13f9af196b7a8046e6c
Krishna Kumar Prajapati <94270945+kkp785216@users.noreply.github.com>
2023-09-08 13:28:45 -0700

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,7 +14,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <div className="flex justify-center items-center min-h-screen flex-col p-6 lg:hidden">
         <h1 className="text-center text-lg font-medium">
           Please view this page at a width greater than 1024px for the best
-          experience.
+          experience. Some changes
         </h1>
       </div>
     </>
